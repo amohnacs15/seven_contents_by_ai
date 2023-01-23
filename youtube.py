987fd635cbd49875c66069efd293209a5a7c3dfb
Adrian Mohnacs <adrian.mohnacs@textnow.com>
2023-01-23 03:26:28 -0500

    consolidation of functions and preliminary upload structure

--- youtube.py
+++ youtube.py
@@ -11,6 +11,7 @@ import openai
 from time import time,sleep
 import textwrap
 import re
+from datetime import datetime
 
 import pathlib
 import pandas as pd
@@ -29,6 +30,12 @@ def open_file(filepath):
 def save_file(filepath, content):
     with open(filepath, 'w', encoding='utf-8') as outfile:
         outfile.write(content)
+
+def remove_file(filepath):
+    if os.path.exists(filepath):
+        os.remove(filepath)
+    else:
+        print("Deletion Failed: The file does not exist")        
 
 #GPT-3 Function        
 def gpt_3 (prompt):
@@ -88,55 +95,7 @@ def mp3_to_transcript(mp3_filename):
     save_file(result_path, yttrans)
     return result_path
 
-# Dropbox functions
-
-def dropbox_connect():
-    """Create a connection to Dropbox."""
-    print("Initializing Dropbox API...") 
-    try:
-        dbx = dropbox.Dropbox(appsecrets.DROPBOX_APP_KEY)
-        print('Dropbox initialized successfully')
-    except AuthError as e:
-        print('Error connecting to Dropbox with access token: ' + str(e))
-    return dbx
-
-def dropbox_upload_file(local_path, local_file, dropbox_file_path):
-    """Upload a file from the local machine to a path in the Dropbox app directory.
-
-    Args:
-        local_path (str): The path to the local file.
-        local_file (str): The name of the local file.
-        dropbox_file_path (str): The path to the file in the Dropbox app directory.
-
-    Example:
-        dropbox_upload_file('.', 'test.csv', '/stuff/test.csv')
-
-    Returns:
-        meta: The Dropbox file metadata.
-    """
-
-    try:
-        # dbx = dropbox_connect()
-        local_file_path = pathlib.Path(local_path) / local_file
-
-        with local_file_path.open("rb") as f:
-            meta = dbx.files_upload(f.read(), dropbox_file_path, mode=dropbox.files.WriteMode("overwrite"))
-
-            return meta
-    except Exception as e:
-        print('Error uploading file to Dropbox: ' + str(e))     
-
-# Initializations
-openai.api_key = appsecrets.OPEN_AI_API_KEY  
-dbx = dropbox_connect()        
-
-#YOUTUBE URL PROMPT HERE   
-youtube_url = input("Please enter your Youtube URL to generate content from: ")
-filename = save_to_mp3(youtube_url)
-transcriptname = mp3_to_transcript(filename)
-
-#MAIN FUNCTION
-if __name__ == '__main__':
+def transcript_to_summary(transcriptname, filename):
     alltext = open_file(transcriptname)
     chunks = textwrap.wrap(alltext, 2500)
     result = list()
@@ -149,61 +108,147 @@ if __name__ == '__main__':
         print('\n\n\n', count, 'out of', len(chunks), 'Compressions', ' : ', summary)
         result.append(summary)
     save_file('outputs/summary_output.txt', '\n\n'.join(result))
-    
+    dropbox_upload_file('outputs', 'summary_output.txt', '/' + filename.replace(".mp3", "") + '/' + 'summary_output.txt')
+
+def source_to_content(filename, feedin_source, prompt_source, type):
+        """Convert a single file of language to another using chat GPT and upload to dropbox
+        
+        Args:
+        feedin_source (str): The path to the file.
+        prompt_source (str): The path for the GPT prompt.
+        type (str): simple categorization to help with naming
+        dropbox_file_path (str): The path to the file in the Dropbox app directory.
+
+        Example:
+            dropbox_upload_file('.', 'test.csv', '/stuff/test.csv')
+
+        Returns: nothing
+        """
+        feedin = open_file(feedin_source)
+        appliedprompt = open_file(prompt_source).replace('<<FEED>>', feedin)
+        finaltext = gpt_3(appliedprompt)
+        
+        print('\n\n\n', type + ' post:\n\n', finaltext)
+
+        save_file('outputs/'+type+'_output.txt', finaltext)
+        dropbox_upload_file('outputs', type + '_output.txt', '/' + filename.replace(".mp3", "") + '/' + type + '_output.txt')
+        remove_file('outputs/'+type+'_output.txt')
+
+# Dropbox functions
+
+def dropbox_connect():
+    """Create a connection to Dropbox."""
+    print("Initializing Dropbox API...") 
+    try:
+        dbx = dropbox.Dropbox(appsecrets.DROPBOX_APP_TOKEN)
+        print('*****Dropbox initialized successfully')
+    except AuthError as e:
+        print('*****Error connecting to Dropbox with access token: ' + str(e))
+    return dbx
+
+def dropbox_upload_file(local_path, local_file, dropbox_file_path):
+    """Upload a file from the local machine to a path in the Dropbox app directory.
+
+    Args:
+        local_path (str): The path to the local file.
+        local_file (str): The name of the local file.
+        dropbox_file_path (str): The path to the file in the Dropbox app directory.
+
+    Example:
+        source_to_content(filename, transcriptname, 'prompts/blog.txt', "blog")
+
+    Returns:
+        meta: The Dropbox file metadata.
+    """
+
+    try:
+        # dbx = dropbox_connect()
+        local_file_path = pathlib.Path(local_path) / local_file
+
+        with local_file_path.open("rb") as f:
+            meta = dbx.files_upload(f.read(), dropbox_file_path, mode=dropbox.files.WriteMode("overwrite"))
+
+            print("*****upload success of " + local_file)
+
+            return meta
+    except Exception as e:
+        print('*****Error uploading file to Dropbox: ' + str(e))     
+
+# Initializations
+openai.api_key = appsecrets.OPEN_AI_API_KEY  
+dbx = dropbox_connect()        
+
+#YOUTUBE URL PROMPT HERE   
+youtube_url = input("Please enter your Youtube URL to generate content from: ")
+filename = save_to_mp3(youtube_url)
+transcriptname = mp3_to_transcript(filename)
+
+#MAIN FUNCTION
+if __name__ == '__main__':
+    transcript_to_summary(transcriptname, filename)
+
+    source_to_content(filename, transcriptname, 'prompts/blog.txt', "blog")
     #Blog Post
-    feedin = open_file(transcriptname)
-    text1 = open_file('prompts/blog.txt').replace('<<FEED>>', feedin)
-    finaltext = gpt_3(text1)
-    print('\n\n\n', 'Blog Post:\n\n', finaltext)
-    save_file('outputs/blog_output.txt', finaltext)
+    # feedin = open_file(transcriptname)
+    # text1 = open_file('prompts/blog.txt').replace('<<FEED>>', feedin)
+    # finaltext = gpt_3(text1)
+    # print('\n\n\n', 'Blog Post:\n\n', finaltext)
+    # save_file('outputs/blog_output.txt', finaltext)
     
+    source_to_content(filename, 'outputs/summary_output.txt', 'prompts/stepguide.txt', "stepguide")
     #Step-by-Step
-    feedin10 = open_file('outputs/summary_output.txt')
-    text20 = open_file('prompts/stepguide.txt').replace('<<FEED>>', feedin10)
-    finaltext10 = gpt_3(text20)
-    print('\n\n\n', 'Step-by-Step Guide:\n\n', finaltext10)
-    save_file('outputs/stepguide_output.txt', finaltext10)
+    # feedin10 = open_file('outputs/summary_output.txt')
+    # text20 = open_file('prompts/stepguide.txt').replace('<<FEED>>', feedin10)
+    # finaltext10 = gpt_3(text20)
+    # print('\n\n\n', 'Step-by-Step Guide:\n\n', finaltext10)
+    # save_file('outputs/stepguide_output.txt', finaltext10)
     
+    source_to_content(filename, 'outputs/summary_output.txt', 'prompts/socialmedia.txt', "socialmedia")
     #Social
-    feedin1 = open_file('outputs/summary_output.txt')
-    text2 = open_file('prompts/socialmedia.txt').replace('<<FEED>>', feedin1)
-    finaltext1 = gpt_3(text2)
-    print('\n\n\n', 'Social Media:\n\n', finaltext1)
-    save_file('outputs/socialmedia_output.txt', finaltext1)
+    # feedin1 = open_file('outputs/summary_output.txt')
+    # text2 = open_file('prompts/socialmedia.txt').replace('<<FEED>>', feedin1)
+    # finaltext1 = gpt_3(text2)
+    # print('\n\n\n', 'Social Media:\n\n', finaltext1)
+    # save_file('outputs/socialmedia_output.txt', finaltext1)
     
+    source_to_content(filename, 'outputs/summary_output.txt', 'prompts/visual.txt', "visual")
     #Visual
-    feedin2 = open_file('outputs/summary_output.txt')
-    text3 = open_file('prompts/visual.txt').replace('<<FEED>>', feedin2)
-    finaltext2 = gpt_3(text3)
-    print('\n\n\n', 'Visual:\n\n', finaltext2)
-    save_file('outputs/visual_output.txt', finaltext2)
+    # feedin2 = open_file('outputs/summary_output.txt')
+    # text3 = open_file('prompts/visual.txt').replace('<<FEED>>', feedin2)
+    # finaltext2 = gpt_3(text3)
+    # print('\n\n\n', 'Visual:\n\n', finaltext2)
+    # save_file('outputs/visual_output.txt', finaltext2)
     
+    source_to_content(filename, 'outputs/summary_output.txt', 'prompts/takeaways.txt', "takeaways")
     #Takeaways
-    feedin3 = open_file('outputs/summary_output.txt')
-    text4 = open_file('prompts/takeaways.txt').replace('<<FEED>>', feedin3)
-    finaltext3 = gpt_3(text4)
-    print('\n\n\n', 'Takeaways:\n\n', finaltext3)
-    save_file('outputs/takeaways_output.txt', finaltext3)
+    # feedin3 = open_file('outputs/summary_output.txt')
+    # text4 = open_file('prompts/takeaways.txt').replace('<<FEED>>', feedin3)
+    # finaltext3 = gpt_3(text4)
+    # print('\n\n\n', 'Takeaways:\n\n', finaltext3)
+    # save_file('outputs/takeaways_output.txt', finaltext3)
     
+    source_to_content(filename, 'outputs/summary_output.txt', 'prompts/script.txt', "youtubescript")
     #Short Video
-    feedin4 = open_file('outputs/summary_output.txt')
-    text5 = open_file('prompts/script.txt').replace('<<SUM>>', feedin4)
-    finaltext4 = gpt_3(text5)
-    print('\n\n\n', 'Youtube Script:\n\n', finaltext4)
-    save_file('outputs/script_output.txt', finaltext4)
+    # feedin4 = open_file('outputs/summary_output.txt')
+    # text5 = open_file('prompts/script.txt').replace('<<SUM>>', feedin4)
+    # finaltext4 = gpt_3(text5)
+    # print('\n\n\n', 'Youtube Script:\n\n', finaltext4)
+    # save_file('outputs/script_output.txt', finaltext4)
     
+    source_to_content(filename, 'outputs/summary_output.txt', 'prompts/story.txt', "story")
     #Story
-    feedin5 = open_file('outputs/summary_output.txt')
-    text6 = open_file('prompts/story.txt').replace('<<FEED>>', feedin5)
-    finaltext5 = gpt_3(text6)
-    print('\n\n\n', 'Story:\n\n', finaltext5)
-    save_file('outputs/story_output.txt', finaltext5)
+    # feedin5 = open_file('outputs/summary_output.txt')
+    # text6 = open_file('prompts/story.txt').replace('<<FEED>>', feedin5)
+    # finaltext5 = gpt_3(text6)
+    # print('\n\n\n', 'Story:\n\n', finaltext5)
+    # save_file('outputs/story_output.txt', finaltext5)
     
+    source_to_content(filename, 'outputs/summary_output.txt', 'prompts/quiz.txt', "quiz")
     #Quiz
-    feedin6 = open_file('outputs/summary_output.txt')
-    text7 = open_file('prompts/quiz.txt').replace('<<FEED>>', feedin6)
-    finaltext6 = gpt_3(text7)
-    print('\n\n\n', 'Quiz:\n\n', finaltext6)
-    save_file('outputs/quiz_output.txt', finaltext6)
+    # feedin6 = open_file('outputs/summary_output.txt')
+    # text7 = open_file('prompts/quiz.txt').replace('<<FEED>>', feedin6)
+    # finaltext6 = gpt_3(text7)
+    # print('\n\n\n', 'Quiz:\n\n', finaltext6)
+    # save_file('outputs/quiz_output.txt', finaltext6)
     
     
