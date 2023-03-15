d915f51697cc79600f667c64819105f981210170
Adrian Mohnacs <amohnacs@gmail.com>
2023-03-15 14:27:36 +0700

    dead end with linkedin since we can't post as company

--- src/content/linkedin_content_repo.py
+++ src/content/linkedin_content_repo.py
@@ -17,7 +17,62 @@ def get_author_from_user(headers):
     urn = user_info['id']
     return f'urn:li:person:{urn}'
 
+def post_linkedin_link_message():
+    #get post from firebase
+
+    credentials = os.path.join('src', 'linkedin_creds.json')
+    access_token = li_auth.auth(credentials) # Authenticate the API
+    gen_headers = li_auth.headers(access_token) # Make the headers to attach to the API call.
+
+    author = get_author_from_user(gen_headers)
+    post_url = 'https://api.linkedin.com/v2/ugcPosts'
+
+    message = '''
+        Interested to automate LinkedIn using #Python and the LinkedIn API? 
+        Read this in-depth Python for #SEO post I wrote.
+    '''
+    link = 'https://www.jcchouinard.com/how-to-use-the-linkedin-api-python/'
+    link_text = 'Complete tutorial using the LinkedIn API'
+    
+    post_data = {
+        "author": author,
+        "lifecycleState": "PUBLISHED",
+        "specificContent": {
+            "com.linkedin.ugc.ShareContent": {
+                "shareCommentary": {
+                    "text": "message"
+                },
+                "shareMediaCategory": "ARTICLE",
+                "media": [
+                    {
+                        "status": "READY",
+                        "description": {
+                            "text": message
+                        },
+                        "originalUrl": link,
+                        "title": {
+                            "text": link_text
+                        }
+                    }
+                ]
+            }
+        },
+        "visibility": {
+            "com.linkedin.ugc.MemberNetworkVisibility": "CONNECTIONS"
+        }
+    }
+    result = make_api_call(
+        post_url, 
+        headers=gen_headers,
+        endpointJson=post_data, 
+        type='POST'
+    )
+    print(f'LINKEDIN {result}')
+    return result
+
 def post_linkedin_user_message( scheduled_datetime_str ):
+    #get post from firebase
+
     credentials = os.path.join('src', 'linkedin_creds.json')
     access_token = li_auth.auth(credentials) # Authenticate the API
     gen_headers = li_auth.headers(access_token) # Make the headers to attach to the API call.
@@ -52,6 +107,6 @@ def post_linkedin_user_message( scheduled_datetime_str ):
 def post_to_linkedin(): 
     return firebase_storage_instance.upload_if_ready(
         PostingPlatform.LINKEDIN, 
-        post_linkedin_user_message,
+        post_linkedin_organization_message,
         True # in testing mode, will post
     )
--- src/content_creator.py
+++ src/content_creator.py
@@ -58,15 +58,14 @@ def get_google_sheets():
 
 # Begin the running of our application
 if __name__ == '__main__':
-    linkedin_repo.post_to_linkedin()
 
     # Quickly process our posts
     # put our post calls here. this will need to be first with the proper implementation
-    # post('Shopify', shopify_content_repo.post_shopify_blog_article())
-    # post('Facebook', fb_content_repo.post_fb_image())
-    # post('Instagram', ig_content_repo.post_ig_media_post())
-    # post('Twitter', twitter_content_repo.post_tweet())
-    # post_youtube_video()
+    post('Shopify', shopify_content_repo.post_shopify_blog_article())
+    post('Facebook', fb_content_repo.post_fb_image())
+    post('Instagram', ig_content_repo.post_ig_media_post())
+    post('Twitter', twitter_content_repo.post_tweet())
+    post_youtube_video()
 
     # Begin our block for long running creation
     # Schedule our content by iterating through each row of sheet
